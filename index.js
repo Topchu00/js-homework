be8e3cebe15b31ade1d5d896320edf09a505a2be
Topchu00 <orozbekovtopchu@gmail.com>
2025-01-14 08:04:48 -0600

    преобразования типов домашка

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-let username = 'Азат?'
-let greeting = 'Привет, как дела'
-console.log(greeting, username);
+let toy = prompt("Введите название игрушки которую хотите купить")
+let toyprice = 400
+let count = prompt("Сколько игрушек вы хотите купить?")
+let sum = `Итоговая стоимость ${count} игрушек ${toy} составляет: ${toyprice * count} сомов`
+alert(String(sum));
 
