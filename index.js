83ea3a8614dfdcd4557665cc09aad60dda256d66
Topchu00 <orozbekovtopchu@gmail.com>
2025-01-14 07:33:26 -0600

    переменные домашка

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
-const height = 5;
+let username = 'Азат?'
+let greeting = 'Привет, как дела'
+console.log(greeting, username);
 
-for (let num = 1; num <= height; num++) {
-  let row = ""; 
-
-  for (let space = 1; space <= height - num; space++) {
-    row += " ";
-  }
-
-  for (let star = 1; star <= 2 * num - 1; star++) {
-    row += "*";
-  }
-
-  console.log(row);
-}
-
-for (let num = height; num >= 1; num--) {
-    let row = ""; 
-  
-    for (let space = 1; space <= height - num; space++) {
-      row += " ";
-    }
-  
-    for (let star = 1; star <= 2 * num - 1; star++) {
-      row += "*";
-    }
-  
-    console.log(row);
-  }
